912ebb206b3c8664d1f3341fb0f7c9daa2aa62b2
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2020-04-08 11:03:17 +0200

    Structure done + authenticate feature added : from now, only person with a JWT (token) can access to the api

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,8 @@ var express         = require('express');
 var   routes          = express.Router();
 var userController  = require('../controllers/user-controller');
 var passport	    = require('passport');
+var todoController  = require('../controllers/todo-controller');
+
 
 routes.get('/', (req, res) => {
     return res.send('Hello, this is the API!');
@@ -12,6 +14,10 @@ routes.post('/login', userController.loginUser);
 // this request is allowed when the person is authenticated
 routes.get('/special', passport.authenticate('jwt', { session: false }), (req, res) => {
     return res.json({ msg: `Hey ${req.user.email}! I open at the close.` });
+});
+
+routes.post('/createTodo',(req,res)=>{
+    todoController.createTodo(req,res)
 });
 
 module.exports = routes;
