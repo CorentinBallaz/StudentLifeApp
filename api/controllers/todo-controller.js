436b9a84e82bbfae4981cce4d77657a053f2219b
WilliamBurillon <William.Burillon@etu.univ-savoie.fr>
2020-04-10 14:53:56 +0200

    todo update, register without email

--- api/controllers/todo-controller.js
+++ api/controllers/todo-controller.js
@@ -6,7 +6,7 @@ var User = require('../models/user')
 exports.createTodo = (req,res)=>{
 
     
-    User.findOne({email:req.body.email},(err,user) =>{
+    User.findOne({_id:req.body.idUser},(err,user) =>{
 
         if (err) {
             return res.status(400).send({ 'msg': err });
@@ -15,14 +15,25 @@ exports.createTodo = (req,res)=>{
         }
         else {
             let newTodo = Todo(req.body);
-            newTodo.save((err,user) =>{
+            newTodo.save((err,todo) =>{
                 if (err) {
                     console.log("pb 2");
                     return res.status(400).json({ 'msg': err });
                 }
-                return res.status(201).json(user);
+
+
+                User.findOneAndUpdate({_id:req.body.idUser},{"$push":{"todos" : todo['_id']}},(err,todo)=>{
+                    if (err) throw err;
+                    res.json({info: 'Success'});
+
+
+                })
+
             });
+
         }
+
+
     })
 
 
@@ -52,7 +63,16 @@ exports.getTodo = (req,res)=>{
 
     })
 
-}
+};
+
+exports.deleteTodo = (req,res)=>{
+    Todo.findOneAndRemove({email : req.params.userMail,label: req.params.label},(err,todo)=>{
+        if (err) throw err;
+
+        res.json({info: 'Success'});
+
+    })
+};
 
 
 
