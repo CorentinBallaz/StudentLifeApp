413bdd4983b63e6da8283ff7d17237820c71764d
Loick-C <loick.combrie1@gmail.com>
2020-05-09 17:48:21 +0200

    onglet notes

--- app/src/app/notes-manager/notes-manager.page.ts
+++ app/src/app/notes-manager/notes-manager.page.ts
@@ -9,17 +9,19 @@ import localeFr from '@angular/common/locales/fr';
 import { ScreensizeService } from '../services/screensize.service';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js'
-
 @Component({
   selector: 'app-notes-manager',
   templateUrl: './notes-manager.page.html',
   styleUrls: ['./notes-manager.page.scss'],
 })
-export class NotesManagerPage implements OnInit {
 
-  isDesktop: boolean;
-  
-  constructor(private screensizeService: ScreensizeService,private todoService: TodoService) {
+export class NotesManagerPage implements OnInit {
+	@ViewChild('barChart',{static: false}) barChart;
+	isDesktop: boolean;
+	ListeUE : Array<any>;
+	bars: any;
+    colorArray: any;
+	constructor(private screensizeService: ScreensizeService) {
 		this.screensizeService.isDesktopView().subscribe(isDesktop => {
 			if (this.isDesktop && !isDesktop) {
 			  // Reload because our routing is out of place
@@ -28,13 +30,48 @@ export class NotesManagerPage implements OnInit {
 	   
 			this.isDesktop = isDesktop;
 		  });
+		this.ListeUE=[{name : 'UE1'},{name : 'UE2'},{name : 'UE3'},{name : 'UE4'},{name : 'UE5'},{name : 'UE6'}];
 	   }
+	ionViewDidEnter() {
+		this.createBarChart();
+	}
+	createBarChart() {
+		this.bars = new Chart(this.barChart.nativeElement, {
+			type: 'horizontalBar',
+			data: {
+				labels: ["AAA"],
+				datasets: [{
+					label: 'TP',
+					data: [20],
+					backgroundColor: "rgba(63,103,126,1)",
+				}, {
+					label: 'TD',
+					data: [20],
+					backgroundColor: "rgba(163,103,126,1)",
+				}, {
+					label: 'CM',
+					data: [12],
+					backgroundColor: "rgba(63,203,226,1)",
+				}]
+			},
+
+			options: {
+				responsive: false,
+				legend: {
+					position: 'right'
+				},
+				scales: {
+					xAxes: [{
+						stacked: true
+					}],
+					yAxes: [{
+						stacked: true
+					}]
+				}
+			}
+		});
+	}
 
   ngOnInit() {
   }
-
-  logout() {
-    this.todoService.logout();
-  }
-
 }
