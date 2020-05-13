2d784e7060b614117dfa03be81abc80863e9aa3c
Loick-C <loick.combrie1@gmail.com>
2020-05-13 19:58:44 +0200

    graph pour les notes

--- app/src/app/notes-manager/notes-manager.module.ts
+++ app/src/app/notes-manager/notes-manager.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { NotesManagerPageRoutingModule } from './notes-manager-routing.module';
 
 import { NotesManagerPage } from './notes-manager.page';
-
+var UEListe = [{name : 'UE1'},{name : 'UE2'},{name : 'UE3'},{name : 'UE4'},{name : 'UE5'},{name : 'UE6'}];
 @NgModule({
   imports: [
     CommonModule,
--- app/src/app/notes-manager/notes-manager.page.ts
+++ app/src/app/notes-manager/notes-manager.page.ts
@@ -9,17 +9,21 @@ import localeFr from '@angular/common/locales/fr';
 import { ScreensizeService } from '../services/screensize.service';
 import { HttpClient } from '@angular/common/http';
 import { Chart } from 'chart.js'
-
 @Component({
   selector: 'app-notes-manager',
   templateUrl: './notes-manager.page.html',
   styleUrls: ['./notes-manager.page.scss'],
 })
-export class NotesManagerPage implements OnInit {
 
-  isDesktop: boolean;
-  
-  constructor(private screensizeService: ScreensizeService,private todoService: TodoService) {
+export class NotesManagerPage implements OnInit {
+	isDesktop: boolean;
+	ListeUE : Array<any>;
+	bars: any;
+	horbar : any;
+	colorArray: any;
+	mixed:any;
+	box:any;
+	constructor(private screensizeService: ScreensizeService) {
 		this.screensizeService.isDesktopView().subscribe(isDesktop => {
 			if (this.isDesktop && !isDesktop) {
 			  // Reload because our routing is out of place
@@ -28,13 +32,160 @@ export class NotesManagerPage implements OnInit {
 	   
 			this.isDesktop = isDesktop;
 		  });
+		this.ListeUE=[{name : 'UE1'},{name : 'UE2'},{name : 'UE3'},{name : 'UE4'},{name : 'UE5'},{name : 'UE6'}];
 	   }
+	ionViewDidEnter() {
+		this.createBarChart();
+		this.createHorChart();
+		this.createboxplot();
+	}
+ 	@ViewChild('barChart',{static: false}) barChart;
+	createBarChart() {
+		this.bars = new Chart(this.barChart.nativeElement, {
+			type: 'horizontalBar',
+			data: {
+				labels: ["AAA"],
+				datasets: [{
+					label: 'DATA1',
+					data: [20],
+					backgroundColor: "rgba(63,103,126,1)",
+				}, {
+					label: 'DATA2',
+					data: [20],
+					backgroundColor: "rgba(163,103,126,1)",
+				}, {
+					label: 'DATA3',
+					data: [12],
+					backgroundColor: "rgba(63,203,226,1)",
+				}]
+			},
+
+			options: {
+				responsive: false,
+				legend: {
+					position: 'right'
+				},
+				scales: {
+					xAxes: [{
+						stacked: true
+					}],
+					yAxes: [{
+						stacked: true
+					}]
+				}
+			}
+		});
+	} 
+	@ViewChild('horChart',{static: false}) horChart;
+	createHorChart() {
+		this.horbar = new Chart(this.horChart.nativeElement, {
+		  type: 'bar',
+		  data: {
+			labels: ['DATA1','DATA2','DATA3'],
+			datasets: [{
+			  label: 'TP',
+			  data: [12, 16, 4],
+			  backgroundColor: '#ddee44',
+			  borderColor: '#ddee44',
+			  borderWidth: 1
+			},
+			{
+			  label: 'TD',
+			  data: [3,6,9],
+			  backgroundColor: '#dd1144', 
+			  borderColor: '#dd1144',
+			  borderWidth: 1
+			},
+			{
+				label: 'CM',
+				data: [9, 12, 15],
+				backgroundColor: "rgba(63,203,226,1)",
+				borderWidth: 1
+			}]
+		  },
+		  options: {
+			scales: {
+				xAxes: [{
+					stacked: true
+				}],
+				yAxes: [{
+					stacked: true
+				}]
+			}
+		}
+		});
+	  }
+
+
+  	@ViewChild('boxPlot',{static: false}) boxPlot;
+	createboxplot(){
+		this.box = new Chart(this.boxPlot.nativeElement, {
+			type: "bar",
+			data: {
+				labels: ['Data1','Data2','Data3'],
+				datasets: [
+				{
+					label: "min",
+					backgroundColor: "rgba(240, 140, 121, 0.0)",
+					borderColor: "rgba(140, 140, 140, 0.0)",
+					borderWidth: 0,
+					data: [
+					10,
+					2,
+					5
+					],
+				},
+				{
+					label: "avg",
+					backgroundColor: "rgba(240, 140, 121, 0.8)",
+					borderColor: "rgba(140, 140, 140, 1.0)",
+					data: [
+					12,
+					8,
+					10
+					],
+				},
+				{
+					label: "max",
+					backgroundColor: "rgba(121, 200, 121, 0.8)",
+					borderColor: "rgba(140, 140, 140, 0.0)",
+					borderWidth: 0,
+					data: [
+					17,
+					15,
+					16
+					],
+				},
+				{label: 'Avg Student',
+				data: [15.5, 17, 20],
+				type: 'line'
+				},
+				]
+			},
+			options: {
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+					displayColors: false,
+				},
+				responsive: true,
+				scales: {
+				xAxes: [
+					{
+					stacked: true,
+					}
+				],
+				yAxes: [
+					{
+					stacked: false,
+					}
+				]
+				}
+			}
+		});
+	}
+
 
   ngOnInit() {
   }
-
-  logout() {
-    this.todoService.logout();
-  }
-
 }
